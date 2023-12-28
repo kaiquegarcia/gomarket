6e02a53cfb1ffb71761e4d712049e57e10f82a04
Kaique Garcia Menezes <contato@kaiquegarcia.dev>
2023-12-28 18:25:35 -0300

    feat: auto-documented enum values

--- internal/enum/unit.go
+++ internal/enum/unit.go
@@ -6,31 +6,31 @@ type Unit float32
 const (
 	// mass
 	MILLIGRAM Unit = 1
-	GRAM      Unit = 1000
-	OUNCE     Unit = 28349.5
-	POUND     Unit = 453592
-	KILOGRAM  Unit = 1000000
-	TONNE     Unit = 1000000000
+	GRAM           = 1000 * MILLIGRAM
+	OUNCE          = 28349.5 * MILLIGRAM
+	POUND          = 453592 * MILLIGRAM
+	KILOGRAM       = 1e+6 * MILLIGRAM
+	TONNE          = 1e+9 * MILLIGRAM
 
 	// volume
 	MILLILITER            Unit = 1
-	AMERICAN_TEASPOON     Unit = 4.92892
-	AMERICAN_TABLESPOON   Unit = 14.7868
-	AMERICAN_LIQUID_OUNCE Unit = 29.5735
-	GLASS                 Unit = 240
-	AMERICAN_PAINT        Unit = 473.176
-	AMERICAN_LIQUID_QUART Unit = 946.353
-	LITER                 Unit = 1000
-	AMERICAN_GALLON       Unit = 3785.41
-	CUBIC_METER           Unit = 1000000
+	AMERICAN_TEASPOON          = 4.92892 * MILLILITER
+	AMERICAN_TABLESPOON        = 14.7868 * MILLILITER
+	AMERICAN_LIQUID_OUNCE      = 29.5735 * MILLILITER
+	GLASS                      = 240 * MILLILITER
+	AMERICAN_PAINT             = 473.176 * MILLILITER
+	AMERICAN_LIQUID_QUART      = 946.353 * MILLILITER
+	LITER                      = 1000 * MILLILITER
+	AMERICAN_GALLON            = 3785.41 * MILLILITER
+	CUBIC_METER                = 1e+6 * MILLILITER
 
 	// length
 	MILLIMETER Unit = 1
-	CENTIMETER Unit = 10
-	INCH       Unit = 25.4
-	FOOT       Unit = 304.8
-	YARD       Unit = 914.4
-	METER      Unit = 1000
-	KILOMETER  Unit = 1000000
-	MILE       Unit = 1609000
+	CENTIMETER      = 10 * MILLIMETER
+	INCH            = 25.4 * MILLIMETER
+	FOOT            = 304.8 * MILLIMETER
+	YARD            = 914.4 * MILLIMETER
+	METER           = 1000 * MILLIMETER
+	KILOMETER       = 1e+6 * MILLIMETER
+	MILE            = 1.609e+6 * MILLIMETER
 )
