1fd44c04306ec783bf94af8a33ff40d2b35f43d7
Kaique Garcia Menezes <contato@kaiquegarcia.dev>
2023-12-29 02:57:16 -0300

    chore: start working on create usecase

--- internal/usecases/product/usecase_create.go
+++ internal/usecases/product/usecase_create.go
@@ -1,5 +1,26 @@
 package product
 
+import (
+	"fmt"
+	"gomarket/internal/entity"
+	"gomarket/internal/usecases/dto"
+	"gomarket/pkg/util"
+)
+
 func (u *usecases) Create() {
 	// TODO
+}
+
+func (u *usecases) createMaterial() int {
+	productDTO := dto.ProductDTO{
+		Materials: make([]entity.Material, 0),
+	}
+	productDTO.Name = util.Ask("what's the material name?")
+	entity, err := u.repository.Insert(productDTO)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Printf("material %s (#%d) created\n", entity.Name, entity.Code)
+	return entity.Code
 }
